6a40cf5346d2e62a67f4fb8fed746fe095ce0840
Rodrigo Torres <torres.dark@gmail.com>
2017-09-21 03:34:26 -0300

    QSqlQueryModel é apenas leitura

--- src/sqlquerymodel.h
+++ src/sqlquerymodel.h
@@ -8,8 +8,6 @@ class SqlQueryModel : public QSqlQueryModel {
 
 public:
   explicit SqlQueryModel(QObject *parent = 0);
-  bool setData(const int row, const int column, const QVariant &value);
-  bool setData(const int row, const QString &column, const QVariant &value);
   bool setHeaderData(const QString &column, const QVariant &value);
   QVariant data(const int row, const QString &column) const;
 

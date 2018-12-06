a0658d64e98ed9ed54d110f0b6028ffdc8aaa33d
Rodrigo Torres <torres.dark@gmail.com>
2018-12-06 01:52:04 -0200

    Melhorias no gráfico

---
+++ src/charttooltip.cpp
@@ -0,0 +1,85 @@
+#include "charttooltip.h"
+
+ChartTooltip::ChartTooltip(QChart *chart) : QGraphicsItem(chart), m_chart(chart) {}
+
+void ChartTooltip::setText(const QString &text) {
+  m_text = QLocale(QLocale::Portuguese).toString(text.toDouble(), 'f', 0);
+  QFontMetrics metrics(m_font);
+  m_textRect = metrics.boundingRect(QRect(0, 0, 150, 150), Qt::AlignLeft, m_text);
+  m_textRect.translate(5, 5);
+  prepareGeometryChange();
+  m_rect = m_textRect.adjusted(-5, 0, 20, 5);
+}
+
+void ChartTooltip::setAnchor(const QPointF point) { m_anchor = point; }
+
+void ChartTooltip::updateGeometry() {
+  prepareGeometryChange();
+  setPos(m_chart->mapToPosition(m_anchor) + QPoint(10, -50));
+}
+
+QRectF ChartTooltip::boundingRect() const {
+  QPointF anchor = mapFromParent(m_chart->mapToPosition(m_anchor));
+  QRectF rect;
+  rect.setLeft(qMin(m_rect.left(), anchor.x()));
+  rect.setRight(qMax(m_rect.right(), anchor.x()));
+  rect.setTop(qMin(m_rect.top(), anchor.y()));
+  rect.setBottom(qMax(m_rect.bottom(), anchor.y()));
+  return rect;
+}
+
+void ChartTooltip::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) {
+  Q_UNUSED(option)
+  Q_UNUSED(widget)
+
+  QPainterPath path;
+  path.addRoundRect(m_rect, 5, 5);
+
+  const QPointF anchor = mapFromParent(m_chart->mapToPosition(m_anchor));
+
+  if (not m_rect.contains(anchor)) {
+    // establish the position of the anchor point in relation to m_rect
+    const bool above = anchor.y() <= m_rect.top();
+    const bool aboveCenter = anchor.y() > m_rect.top() and anchor.y() <= m_rect.center().y();
+    const bool belowCenter = anchor.y() > m_rect.center().y() and anchor.y() <= m_rect.bottom();
+    const bool below = anchor.y() > m_rect.bottom();
+
+    const bool onLeft = anchor.x() <= m_rect.left();
+    const bool leftOfCenter = anchor.x() > m_rect.left() and anchor.x() <= m_rect.center().x();
+    const bool rightOfCenter = anchor.x() > m_rect.center().x() and anchor.x() <= m_rect.right();
+    const bool onRight = anchor.x() > m_rect.right();
+
+    // get the nearest m_rect corner.
+    const qreal x = (onRight + rightOfCenter) * m_rect.width();
+    const qreal y = (below + belowCenter) * m_rect.height();
+    const bool cornerCase = (above and onLeft) or (above and onRight) or (below and onLeft) or (below and onRight);
+    const bool vertical = qAbs(anchor.x() - x) > qAbs(anchor.y() - y);
+
+    const qreal x1 = x + leftOfCenter * 10 - rightOfCenter * 20 + cornerCase * !vertical * (onLeft * 10 - onRight * 20);
+    const qreal y1 = y + aboveCenter * 10 - belowCenter * 20 + cornerCase * vertical * (above * 10 - below * 20);
+
+    const qreal x2 = x + leftOfCenter * 20 - rightOfCenter * 10 + cornerCase * !vertical * (onLeft * 20 - onRight * 10);
+    const qreal y2 = y + aboveCenter * 20 - belowCenter * 10 + cornerCase * vertical * (above * 20 - below * 10);
+
+    path.moveTo(x1, y1 + 5);
+    path.lineTo(anchor);
+    path.lineTo(x2, y2 + 5);
+    path = path.simplified();
+  }
+
+  painter->setBrush(QColor(0, 0, 0));
+  painter->setPen(QColor(255, 255, 255));
+  painter->drawPath(path);
+  painter->drawText(m_textRect.bottomLeft(), "R$ " + m_text);
+}
+
+void ChartTooltip::mousePressEvent(QGraphicsSceneMouseEvent *event) { event->setAccepted(true); }
+
+void ChartTooltip::mouseMoveEvent(QGraphicsSceneMouseEvent *event) {
+  if (event->buttons() & Qt::LeftButton) {
+    setPos(mapToParent(event->pos() - event->buttonDownPos(Qt::LeftButton)));
+    event->setAccepted(true);
+  } else {
+    event->setAccepted(false);
+  }
+}
--- src/widgetgraficos.cpp
+++ src/widgetgraficos.cpp
@@ -1,14 +1,16 @@
 #include <QSqlError>
 #include <QSqlQuery>
-#include <QtCharts>
 
 #include "application.h"
 #include "ui_widgetgraficos.h"
 #include "widgetgraficos.h"
 
-using namespace QtCharts;
-
-WidgetGraficos::WidgetGraficos(QWidget *parent) : QWidget(parent), ui(new Ui::WidgetGraficos) { ui->setupUi(this); }
+WidgetGraficos::WidgetGraficos(QWidget *parent) : QWidget(parent), ui(new Ui::WidgetGraficos) {
+  ui->setupUi(this);
+  connect(ui->checkBox, &QCheckBox::toggled, this, &WidgetGraficos::on_checkBox_toggled);
+  connect(ui->comboBoxTheme, QOverload<int>::of(&QComboBox::currentIndexChanged), this, &WidgetGraficos::on_comboBox_currentIndexChanged);
+  connect(ui->pushButtonCleanTooltips, &QPushButton::clicked, this, &WidgetGraficos::on_pushButtonCleanTooltips_clicked);
+}
 
 WidgetGraficos::~WidgetGraficos() { delete ui; }
 
@@ -16,99 +18,169 @@ void WidgetGraficos::resetTables() {}
 
 void WidgetGraficos::updateTables() {
   if (not isSet) {
-    QSqlQuery queryChart;
-
-    if (not queryChart.exec(
-            "SELECT dia,@s1:=@s1 + jane AS jane,@s2:=@s2 + feve AS feve,@s3:=@s3 + marc AS marc,@s4:=@s4 + abri AS abri,@s5:=@s5 + maio AS maio,@s6:=@s6 + junh AS junh,@s7:=@s7 + julh AS julh,"
-            "@s8:=@s8 + agos AS agos,@s9:=@s9 + sete AS sete,@s10:=@s10 + outu AS outu,@s11:=@s11 + nove AS nove,@s12:=@s12 + deze AS deze FROM view_relatorio_temp v JOIN (SELECT @s1:=0,"
-            "@s2:=0,@s3:=0,@s4:=0,@s5:=0,@s6:=0,@s7:=0,@s8:=0,@s9:=0,@s10:=0,@s11:=0,@s12:=0) s")) {
+    if (not queryChart.exec("SELECT dia, @s12:=@s12 + mes12 AS mes12, @s11:=@s11 + mes11 AS mes11, @s10:=@s10 + mes10 AS mes10, @s9:=@s9 + mes9 AS mes9, @s8:=@s8 + mes8 AS mes8, @s7:=@s7 + mes7 AS "
+                            "mes7, @s6:=@s6 + mes6 AS mes6, @s5:=@s5 + mes5 AS mes5, @s4:=@s4 + mes4 AS mes4, @s3:=@s3 + mes3 AS mes3, @s2:=@s2 + mes2 AS mes2, @s1:=@s1 + mes1 AS mes1, @s0:=@s0 + "
+                            "mes0 AS mes0 FROM view_relatorio_temp2 v JOIN (SELECT @s12:=0, @s11:=0,@s10:=0,@s9:=0,@s8:=0,@s7:=0,@s6:=0,@s5:=0,@s4:=0,@s3:=0,@s2:=0,@s1:=0,@s0:=0) s")) {
       return qApp->enqueueError("Erro lendo tabela: " + queryChart.lastError().text());
     }
 
+    const QDate now = QDate::currentDate();
+
+    series12.setName(now.addMonths(-12).toString("MMM"));
+    series11.setName(now.addMonths(-11).toString("MMM"));
+    series10.setName(now.addMonths(-10).toString("MMM"));
+    series9.setName(now.addMonths(-9).toString("MMM"));
+    series8.setName(now.addMonths(-8).toString("MMM"));
+    series7.setName(now.addMonths(-7).toString("MMM"));
+    series6.setName(now.addMonths(-6).toString("MMM"));
+    series5.setName(now.addMonths(-5).toString("MMM"));
+    series4.setName(now.addMonths(-4).toString("MMM"));
+    series3.setName(now.addMonths(-3).toString("MMM"));
+    series2.setName(now.addMonths(-2).toString("MMM"));
+    series1.setName(now.addMonths(-1).toString("MMM"));
+    series0.setName(now.toString("MMM"));
+
     int dia = 1;
 
-    QLineSeries *seriesJan = new QLineSeries(this);
-    seriesJan->setName("Jan");
-    QLineSeries *seriesFev = new QLineSeries(this);
-    seriesFev->setName("Fev");
-    QLineSeries *seriesMar = new QLineSeries(this);
-    seriesMar->setName("Mar");
-    QLineSeries *seriesAbr = new QLineSeries(this);
-    seriesAbr->setName("Abr");
-    QLineSeries *seriesMai = new QLineSeries(this);
-    seriesMai->setName("Mai");
-    QLineSeries *seriesJun = new QLineSeries(this);
-    seriesJun->setName("Jun");
-    QLineSeries *seriesJul = new QLineSeries(this);
-    seriesJul->setName("Jul");
-    QLineSeries *seriesAgo = new QLineSeries(this);
-    seriesAgo->setName("Ago");
-    QLineSeries *seriesSet = new QLineSeries(this);
-    seriesSet->setName("Set");
-    QLineSeries *seriesOut = new QLineSeries(this);
-    seriesOut->setName("Out");
-    QLineSeries *seriesNov = new QLineSeries(this);
-    seriesNov->setName("Nov");
-    QLineSeries *seriesDez = new QLineSeries(this);
-    seriesDez->setName("Dez");
-
     while (queryChart.next()) {
-
-      seriesJan->append(dia, queryChart.value("jane").toDouble());
-      seriesFev->append(dia, queryChart.value("feve").toDouble());
-      seriesMar->append(dia, queryChart.value("marc").toDouble());
-      seriesAbr->append(dia, queryChart.value("abri").toDouble());
-      seriesMai->append(dia, queryChart.value("maio").toDouble());
-      seriesJun->append(dia, queryChart.value("junh").toDouble());
-      seriesJul->append(dia, queryChart.value("julh").toDouble());
-      seriesAgo->append(dia, queryChart.value("agos").toDouble());
-      seriesSet->append(dia, queryChart.value("sete").toDouble());
-      seriesOut->append(dia, queryChart.value("outu").toDouble());
-      seriesNov->append(dia, queryChart.value("nove").toDouble());
-      seriesDez->append(dia, queryChart.value("deze").toDouble());
+      series12.append(dia, queryChart.value("mes12").toDouble());
+      series11.append(dia, queryChart.value("mes11").toDouble());
+      series10.append(dia, queryChart.value("mes10").toDouble());
+      series9.append(dia, queryChart.value("mes9").toDouble());
+      series8.append(dia, queryChart.value("mes8").toDouble());
+      series7.append(dia, queryChart.value("mes7").toDouble());
+      series6.append(dia, queryChart.value("mes6").toDouble());
+      series5.append(dia, queryChart.value("mes5").toDouble());
+      series4.append(dia, queryChart.value("mes4").toDouble());
+      series3.append(dia, queryChart.value("mes3").toDouble());
+      series2.append(dia, queryChart.value("mes2").toDouble());
+      series1.append(dia, queryChart.value("mes1").toDouble());
+      series0.append(dia, queryChart.value("mes0").toDouble());
 
       dia++;
     }
 
-    QChart *chart = new QChart();
-    chart->addSeries(seriesJan);
-    chart->addSeries(seriesFev);
-    chart->addSeries(seriesMar);
-    chart->addSeries(seriesAbr);
-    chart->addSeries(seriesMai);
-    chart->addSeries(seriesJun);
-    chart->addSeries(seriesJul);
-    chart->addSeries(seriesAgo);
-    chart->addSeries(seriesSet);
-    chart->addSeries(seriesOut);
-    chart->addSeries(seriesNov);
-    chart->addSeries(seriesDez);
-    chart->createDefaultAxes();
-    chart->setTheme(QChart::ChartThemeBlueCerulean);
-    chart->setTitle("Acumulado mensal");
-    chart->legend()->setAlignment(Qt::AlignBottom);
-    chart->setLocalizeNumbers(true);
-    static_cast<QValueAxis *>(chart->axisY())->setLabelFormat("R$ %.0f");
-    static_cast<QValueAxis *>(chart->axisX())->setLabelFormat("%.0f");
-    static_cast<QValueAxis *>(chart->axisY())->setTickCount(10);
-    static_cast<QValueAxis *>(chart->axisX())->setTickCount(31);
+    chart.addSeries(&series12);
+    chart.addSeries(&series11);
+    chart.addSeries(&series10);
+    chart.addSeries(&series9);
+    chart.addSeries(&series8);
+    chart.addSeries(&series7);
+    chart.addSeries(&series6);
+    chart.addSeries(&series5);
+    chart.addSeries(&series4);
+    chart.addSeries(&series3);
+    chart.addSeries(&series2);
+    chart.addSeries(&series1);
+    chart.addSeries(&series0);
+    chart.createDefaultAxes();
+    chart.setTheme(QChart::ChartThemeLight);
+    chart.setTitle("Acumulado mensal");
+    chart.legend()->setAlignment(Qt::AlignBottom);
+    chart.setLocalizeNumbers(true);
+    static_cast<QValueAxis *>(chart.axisY())->setLabelFormat("R$ %.0f");
+    static_cast<QValueAxis *>(chart.axisX())->setLabelFormat("%.0f");
+    static_cast<QValueAxis *>(chart.axisY())->setTickCount(10);
+    static_cast<QValueAxis *>(chart.axisX())->setTickCount(31);
 
-    QChartView *chartView = new QChartView(chart, this);
-    chartView->setRenderHint(QPainter::Antialiasing);
-    chartView->setRubberBand(QChartView::RectangleRubberBand);
+    const auto markers = chart.legend()->markers();
+
+    for (const auto marker : markers) { connect(marker, &QLegendMarker::clicked, this, &WidgetGraficos::handleMarkerClicked); }
+
+    chartView = new ChartView(&chart, this);
 
     layout()->addWidget(chartView);
 
-    // NOTE: fazer o mes atual ate o dia corrente
-    // fazer o mes atual com a linha em bold
-    // fazer o mesmo mes do ano anterior em bold
-    // fazer uma linha diferente com a media
-
     isSet = true;
+
+    return;
+  }
+
+  if (isSet) {
+    series12.clear();
+    series11.clear();
+    series10.clear();
+    series9.clear();
+    series8.clear();
+    series7.clear();
+    series6.clear();
+    series5.clear();
+    series4.clear();
+    series3.clear();
+    series2.clear();
+    series1.clear();
+    series0.clear();
+
+    if (not queryChart.exec(queryChart.lastQuery())) { return qApp->enqueueError("Erro lendo tabela: " + queryChart.lastError().text()); }
+
+    int dia = 1;
+
+    while (queryChart.next()) {
+      series12.append(dia, queryChart.value("mes12").toDouble());
+      series11.append(dia, queryChart.value("mes11").toDouble());
+      series10.append(dia, queryChart.value("mes10").toDouble());
+      series9.append(dia, queryChart.value("mes9").toDouble());
+      series8.append(dia, queryChart.value("mes8").toDouble());
+      series7.append(dia, queryChart.value("mes7").toDouble());
+      series6.append(dia, queryChart.value("mes6").toDouble());
+      series5.append(dia, queryChart.value("mes5").toDouble());
+      series4.append(dia, queryChart.value("mes4").toDouble());
+      series3.append(dia, queryChart.value("mes3").toDouble());
+      series2.append(dia, queryChart.value("mes2").toDouble());
+      series1.append(dia, queryChart.value("mes1").toDouble());
+      series0.append(dia, queryChart.value("mes0").toDouble());
+
+      dia++;
+    }
   }
 }
 
-// TODO: hover tooltip
-// TODO: fix view to select only current year
+void WidgetGraficos::handleMarkerClicked() {
+  QLegendMarker *marker = qobject_cast<QLegendMarker *>(sender());
+
+  toggleMarker(marker);
+}
+
+void WidgetGraficos::toggleMarker(QLegendMarker *marker) {
+  switch (marker->type()) {
+  case QLegendMarker::LegendMarkerTypeXY: {
+    marker->series()->setVisible(not marker->series()->isVisible());
+
+    // Turn legend marker back to visible, since hiding series also hides the marker and we don't want it to happen now.
+    marker->setVisible(true);
+
+    const qreal alpha = marker->series()->isVisible() ? 1.0 : 0.5;
+
+    QColor color = marker->labelBrush().color();
+    color.setAlphaF(alpha);
+    marker->setLabelBrush(QBrush(color));
+
+    QColor color2 = marker->brush().color();
+    color2.setAlphaF(alpha);
+    marker->setBrush(QBrush(color2));
+
+    QColor color3 = marker->pen().color();
+    color3.setAlphaF(alpha);
+    marker->setPen(QPen(color3));
+
+    break;
+  }
+  default: { break; }
+  }
+}
+
+void WidgetGraficos::on_checkBox_toggled() {
+  const auto markers = chart.legend()->markers();
+
+  for (const auto marker : markers) { toggleMarker(marker); }
+}
+
+void WidgetGraficos::on_comboBox_currentIndexChanged(int index) { chart.setTheme(static_cast<QChart::ChartTheme>(index)); }
+
+void WidgetGraficos::on_pushButtonCleanTooltips_clicked() { chartView->removeTooltips(); }
+
 // TODO: parameterize view to select different years
-// TODO: load chart theme depending on app theme
+// NOTE: fazer o mes atual ate o dia corrente
+// fazer o mes atual com a linha em bold
+// fazer o mesmo mes do ano anterior em bold
+// fazer uma linha diferente com a media

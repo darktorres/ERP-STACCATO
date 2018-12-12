61b55aab261011e1d7a7c8af5cafc81b87b21439
Rodrigo Torres <torres.dark@gmail.com>
2018-12-12 02:33:51 -0200

    Only center messages on MainWindow if there is one

--- src/mainwindow.h
+++ src/mainwindow.h
@@ -14,6 +14,7 @@ class MainWindow final : public QMainWindow {
 public:
   explicit MainWindow(QWidget *parent = nullptr);
   ~MainWindow();
+  auto showMessages(const QStringList &errorQueue, const QStringList &warningQueue, const QStringList &informationQueue) -> void;
   auto updateTables() -> void;
 
 private:

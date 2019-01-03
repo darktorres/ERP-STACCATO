1acd72bc95824113afdd42689d930162b23af7ba
Rodrigo Torres <torres.dark@gmail.com>
2019-01-03 18:00:10 -0200

    Não apenas desfazer consumo mas desvincular também da compra

--- src/widgetcompraoc.h
+++ src/widgetcompraoc.h
@@ -27,8 +27,7 @@ private:
   SqlRelationalTableModel modelProduto;
   Ui::WidgetCompraOC *ui;
   // methods
-  auto desfazerConsumo(const int row, const int idVendaConsumo) -> bool;
-  auto desvincularVenda(const int row) -> bool;
+  auto desfazerConsumo(const int row) -> bool;
   auto montaFiltro() -> void;
   auto on_lineEditBusca_textChanged(const QString &) -> void;
   auto on_pushButtonDanfe_clicked() -> void;

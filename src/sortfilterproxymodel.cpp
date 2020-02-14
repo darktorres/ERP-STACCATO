089556e9a903a7354fcdab99d4c9958d0747e8e6
Rodrigo Torres <torres.dark@gmail.com>
2020-02-14 05:08:16 -0300

    Desativar ordenação dinâmica para poder alterar o model

--- src/sortfilterproxymodel.cpp
+++ src/sortfilterproxymodel.cpp
@@ -1,3 +1,6 @@
 #include "sortfilterproxymodel.h"
 
-SortFilterProxyModel::SortFilterProxyModel(QAbstractItemModel *model, QObject *parent) : QSortFilterProxyModel(parent) { setSourceModel(model); }
+SortFilterProxyModel::SortFilterProxyModel(QAbstractItemModel *model, QObject *parent) : QSortFilterProxyModel(parent) {
+  setSourceModel(model);
+  setDynamicSortFilter(false);
+}

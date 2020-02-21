25242d555d57221cda5d91684779dc1e9242c9dc
Rodrigo Torres <torres.dark@gmail.com>
2020-02-21 16:23:46 -0300

    Salvar no banco de dados mensagens de erro

--- src/log.h
+++ src/log.h
@@ -6,5 +6,5 @@ class Log final {
 
 public:
   Log() = delete;
-  static auto createLog(const QString &message) -> bool;
+  static auto createLog(const QString &message, const bool silent = false) -> bool;
 };

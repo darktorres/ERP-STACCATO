38f6bd9b4e9079404030d336f2c95bff654541d2
Rodrigo Torres <torres.dark@gmail.com>
2020-05-25 14:03:00 -0300

    Faltava a opção de cancelado no status do financeiro

--- src/venda.cpp
+++ src/venda.cpp
@@ -1150,6 +1150,7 @@ bool Venda::cancelamento() {
 void Venda::on_pushButtonCancelamento_clicked() {
   // TODO: perguntar e salvar motivo do cancelamento
   // TODO: caso haja agendamento de entrega cancelar o agendamento primeiro?
+  // TODO: mudar o statusFinanceiro para cancelado tambem
 
   // caso pedido nao seja do mes atual, bloquear se nao estiver no primeiro dia util
   const QDate dataVenda = data("data").toDate();

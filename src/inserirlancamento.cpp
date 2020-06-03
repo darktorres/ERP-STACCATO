27edaa0667e25ff6e1b4e5df7c470db62a28dcb5
Rodrigo Torres <torres.dark@gmail.com>
2020-06-03 12:35:54 -0300

    Preencher os campos igual na tela de contas

--- src/inserirlancamento.cpp
+++ src/inserirlancamento.cpp
@@ -15,17 +15,17 @@
 #include <QDebug>
 #include <QMessageBox>
 #include <QSqlError>
+#include <QSqlQuery>
+#include <QtMath>
 
 InserirLancamento::InserirLancamento(const Tipo tipo, QWidget *parent) : QDialog(parent), tipo(tipo), ui(new Ui::InserirLancamento) {
   ui->setupUi(this);
 
-  connect(ui->pushButtonCriarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonCriarLancamento_clicked);
-  connect(ui->pushButtonDuplicarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonDuplicarLancamento_clicked);
-  connect(ui->pushButtonSalvar, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonSalvar_clicked);
-
   setWindowFlags(Qt::Window);
 
   setupTables();
+
+  setConnections();
 }
 
 InserirLancamento::~InserirLancamento() { delete ui; }
@@ -40,10 +40,16 @@ void InserirLancamento::setupTables() {
   modelContaPagamento.setHeaderData("nfe", "NFe");
   modelContaPagamento.setHeaderData("valor", "R$");
   modelContaPagamento.setHeaderData("tipo", "Tipo");
+  modelContaPagamento.setHeaderData("parcela", "Parcela");
   modelContaPagamento.setHeaderData("dataPagamento", "Vencimento");
   modelContaPagamento.setHeaderData("observacao", "Obs.");
   modelContaPagamento.setHeaderData("status", "Status");
+  modelContaPagamento.setHeaderData("dataRealizado", "Data Realizado");
+  modelContaPagamento.setHeaderData("valorReal", "R$ Real");
+  modelContaPagamento.setHeaderData("tipoReal", "Tipo Real");
+  modelContaPagamento.setHeaderData("parcelaReal", "Parcela Real");
   modelContaPagamento.setHeaderData("idConta", "Conta");
+  modelContaPagamento.setHeaderData("tipoDet", "Tipo Det");
   modelContaPagamento.setHeaderData("grupo", "Grupo");
   modelContaPagamento.setHeaderData("subGrupo", "SubGrupo");
 
@@ -60,21 +66,17 @@ void InserirLancamento::setupTables() {
   ui->table->setItemDelegateForColumn("grupo", new ComboBoxDelegate(ComboBoxDelegate::Tipo::Grupo, this));
   ui->table->setItemDelegateForColumn("contraParte", new LineEditDelegate(LineEditDelegate::Tipo::ContraPartePagar, this));
   ui->table->setItemDelegateForColumn("dataPagamento", new DateFormatDelegate(this));
+  ui->table->setItemDelegateForColumn("dataRealizado", new DateFormatDelegate(this));
   // TODO: 5colocar lineEditDelegate para subgrupo
 
   ui->table->setPersistentColumns({"idLoja", "tipo", "grupo", "status", "idConta"});
 
-  ui->table->hideColumn("parcela");
-  ui->table->hideColumn("dataRealizado");
-  ui->table->hideColumn("valorReal");
-  ui->table->hideColumn("tipoReal");
-  ui->table->hideColumn("parcelaReal");
-  ui->table->hideColumn("tipoDet");
   ui->table->hideColumn("centroCusto");
   ui->table->hideColumn("idPagamento");
 
   if (tipo == Tipo::Pagar) {
     ui->table->hideColumn("idCompra");
+    ui->table->hideColumn("idCnab");
     ui->table->hideColumn("idNFe");
   }
 
@@ -89,10 +91,16 @@ void InserirLancamento::setupTables() {
 }
 
 void InserirLancamento::on_pushButtonCriarLancamento_clicked() {
-  const int newRow = modelContaPagamento.insertRowAtEnd();
+  unsetConnections();
 
-  if (not modelContaPagamento.setData(newRow, "status", "PENDENTE")) { return; }
-  if (not modelContaPagamento.setData(newRow, "dataEmissao", qApp->serverDate())) { return; }
+  [&] {
+    const int newRow = modelContaPagamento.insertRowAtEnd();
+
+    if (not modelContaPagamento.setData(newRow, "status", "PENDENTE")) { return; }
+    if (not modelContaPagamento.setData(newRow, "dataEmissao", qApp->serverDate())) { return; }
+  }();
+
+  setConnections();
 }
 
 void InserirLancamento::on_pushButtonSalvar_clicked() {
@@ -124,6 +132,22 @@ bool InserirLancamento::verifyFields() {
   return true;
 }
 
+void InserirLancamento::setConnections() {
+  const auto connectionType = static_cast<Qt::ConnectionType>(Qt::AutoConnection | Qt::UniqueConnection);
+
+  connect(ui->pushButtonCriarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonCriarLancamento_clicked, connectionType);
+  connect(ui->pushButtonDuplicarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonDuplicarLancamento_clicked, connectionType);
+  connect(ui->pushButtonSalvar, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonSalvar_clicked, connectionType);
+  connect(ui->table->model(), &QAbstractItemModel::dataChanged, this, &InserirLancamento::preencher, connectionType);
+}
+
+void InserirLancamento::unsetConnections() {
+  disconnect(ui->pushButtonCriarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonCriarLancamento_clicked);
+  disconnect(ui->pushButtonDuplicarLancamento, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonDuplicarLancamento_clicked);
+  disconnect(ui->pushButtonSalvar, &QPushButton::clicked, this, &InserirLancamento::on_pushButtonSalvar_clicked);
+  disconnect(ui->table->model(), &QAbstractItemModel::dataChanged, this, &InserirLancamento::preencher);
+}
+
 void InserirLancamento::on_pushButtonDuplicarLancamento_clicked() {
   const auto list = ui->table->selectionModel()->selectedRows();
 
@@ -145,4 +169,29 @@ void InserirLancamento::on_pushButtonDuplicarLancamento_clicked() {
   }
 }
 
-// TODO: ao lançar um pagamento direto como 'PAGO' copiar os valores para valorReal, dataRealizado
+void InserirLancamento::preencher(const QModelIndex &index) {
+  unsetConnections();
+
+  [&] {
+    const int row = index.row();
+
+    if (index.column() == ui->table->columnIndex("dataRealizado")) {
+      const int contaSantander = 3;
+      const int contaItau = 33;
+
+      const int idConta = (tipo == Tipo::Receber and modelContaPagamento.data(row, "tipo").toString().contains("BOLETO")) ? contaItau : contaSantander;
+
+      if (modelContaPagamento.data(row, "idConta").toInt() == 0) {
+        if (not modelContaPagamento.setData(row, "idConta", idConta)) { return; }
+      }
+
+      if (not modelContaPagamento.setData(row, "status", (tipo == Tipo::Receber) ? "RECEBIDO" : "PAGO")) { return; }
+      if (not modelContaPagamento.setData(row, "valorReal", modelContaPagamento.data(row, "valor"))) { return; }
+      if (not modelContaPagamento.setData(row, "tipoReal", modelContaPagamento.data(row, "tipo"))) { return; }
+      if (not modelContaPagamento.setData(row, "parcelaReal", modelContaPagamento.data(row, "parcela"))) { return; }
+      if (not modelContaPagamento.setData(row, "centroCusto", modelContaPagamento.data(row, "idLoja"))) { return; }
+    }
+  }();
+
+  setConnections();
+}

c61cdb57b556d575b2dca637988481c6c9d569f0
Rodrigo Torres <torres.dark@gmail.com>
2020-06-07 21:10:25 -0300

    Não alterar versão no .pro para não invalidar o PCH

--- src/application.cpp
+++ src/application.cpp
@@ -16,7 +16,7 @@ Application::Application(int &argc, char **argv, int) : QApplication(argc, argv)
   setOrganizationName("Staccato");
   setApplicationName("ERP");
   setWindowIcon(QIcon("Staccato.ico"));
-  setApplicationVersion(APP_VERSION);
+  setApplicationVersion("0.8.94");
   setStyle("Fusion");
 
   readSettingsFile();

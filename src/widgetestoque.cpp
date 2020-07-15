60eb77a72caa4de354274bdd0a1505cb91b2d6e2
Rodrigo Torres <torres.dark@gmail.com>
2020-07-15 19:22:10 -0300

    Usar o custo da NFe no lugar do custo da tabela de produtos

--- src/widgetestoque.cpp
+++ src/widgetestoque.cpp
@@ -169,13 +169,13 @@ void WidgetEstoque::on_pushButtonRelatorio_clicked() {
   modelContabil.setQuery(
       "SELECT e.idEstoque, GROUP_CONCAT(DISTINCT n.cnpjDest SEPARATOR ',') AS cnpjDest, e.status, p.fornecedor, e.descricao, e.quant + COALESCE(ehc.contabil, 0) + ajuste AS contabil, "
       "e.restante AS disponivel, e.un AS unEst, p.un AS unProd, (e.quant + COALESCE(ehc.contabil, 0) + ajuste) / p.quantCaixa AS Caixas, e.lote, e.local, e.bloco, e.codComercial, "
-      "e.ncm, e.cstICMS, e.pICMS, e.cstIPI, e.cstPIS, e.cstCOFINS, GROUP_CONCAT(DISTINCT n.numeroNFe SEPARATOR ', ') AS nfe, p.custo AS custoUnit, p.precoVenda AS precoVendaUnit, p.custo * (e.quant "
-      "+ COALESCE(ehc.contabil, 0) + ajuste) AS custo, p.precoVenda * (e.quant + COALESCE(ehc.contabil, 0) + ajuste) AS precoVenda FROM estoque e LEFT JOIN (SELECT ehc.idEstoque, SUM(ehc.quant) AS "
-      "contabil, SUM(IF(vp.status != 'DEVOLVIDO ESTOQUE', vp.quant, 0)) AS consumoVenda FROM estoque_has_consumo ehc LEFT JOIN venda_has_produto2 vp ON ehc.idVendaProduto2 = vp.idVendaProduto2 WHERE "
-      "(vp.dataRealEnt < '" +
+      "e.ncm, e.cstICMS, e.pICMS, e.cstIPI, e.cstPIS, e.cstCOFINS, GROUP_CONCAT(DISTINCT n.numeroNFe SEPARATOR ', ') AS nfe, e.valorUnid AS custoUnit, p.precoVenda AS precoVendaUnit, e.valorUnid * "
+      "(e.quant + COALESCE(ehc.contabil, 0) + ajuste) AS custo, p.precoVenda * (e.quant + COALESCE(ehc.contabil, 0) + ajuste) AS precoVenda FROM estoque e LEFT JOIN (SELECT ehc.idEstoque, "
+      "SUM(ehc.quant) AS contabil, SUM(IF(vp.status != 'DEVOLVIDO ESTOQUE', vp.quant, 0)) AS consumoVenda FROM estoque_has_consumo ehc LEFT JOIN venda_has_produto2 vp ON ehc.idVendaProduto2 = "
+      "vp.idVendaProduto2 WHERE (vp.dataRealEnt < '" +
       data +
       "') AND ehc.status != 'CANCELADO' GROUP BY ehc.idEstoque) ehc ON e.idEstoque = ehc.idEstoque LEFT JOIN estoque_has_compra ehc2 ON e.idEstoque = ehc2.idEstoque LEFT JOIN "
-      "pedido_fornecedor_has_produto2 pf ON pf.idPedido2 = ehc2.idPedido2 LEFT JOIN nfe n ON e.idNFe = n.idNFe LEFT JOIN produto p ON e.idProduto = p.idProduto WHERE e.status = 'ESTOQUE' AND "
+      "pedido_fornecedor_has_produto2 pf2 ON pf2.idPedido2 = ehc2.idPedido2 LEFT JOIN nfe n ON e.idNFe = n.idNFe LEFT JOIN produto p ON e.idProduto = p.idProduto WHERE e.status = 'ESTOQUE' AND "
       "e.created < '" +
       data + "' GROUP BY e.idEstoque HAVING contabil > 0");
 
@@ -236,3 +236,4 @@ bool WidgetEstoque::gerarExcel(const QString &arquivoModelo, const QString &file
 // TODO: terminar de arrumar relatorio estoque
 // TODO: [Conrado] colocar filtro/tela para buscar por pedido e mostrar os estoques em que foi consumido
 // TODO: fix fulltext indexes (put match against inside subquery)
+// TODO: criar um segundo relatorio para os gerentes sem o custo

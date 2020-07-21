311baa5906edb10fc5553153a205c17b1d2dd39d
Rodrigo Torres <torres.dark@gmail.com>
2020-07-21 15:52:16 -0300

    Tela de produtos estoque

--- src/widgetestoque.cpp
+++ src/widgetestoque.cpp
@@ -4,6 +4,7 @@
 #include "application.h"
 #include "doubledelegate.h"
 #include "estoque.h"
+#include "searchdialogproxymodel.h"
 #include "sortfilterproxymodel.h"
 #include "usersession.h"
 #include "xlsxdocument.h"
@@ -22,9 +23,12 @@ void WidgetEstoque::setConnections() {
 
   connect(ui->lineEditBusca, &QLineEdit::textChanged, this, &WidgetEstoque::escolheFiltro, connectionType);
   connect(ui->pushButtonRelatorio, &QPushButton::clicked, this, &WidgetEstoque::on_pushButtonRelatorio_clicked, connectionType);
+  connect(ui->radioButtonEstoque, &QRadioButton::toggled, this, &WidgetEstoque::on_radioButtonEstoque_toggled, connectionType);
   connect(ui->radioButtonEstoqueContabil, &QRadioButton::clicked, this, &WidgetEstoque::montaFiltroContabil, connectionType);
   connect(ui->radioButtonEstoqueZerado, &QRadioButton::clicked, this, &WidgetEstoque::montaFiltro, connectionType);
   connect(ui->radioButtonMaior, &QRadioButton::clicked, this, &WidgetEstoque::montaFiltro, connectionType);
+  connect(ui->radioButtonStaccatoOFF, &QRadioButton::toggled, this, &WidgetEstoque::on_radioButtonStaccatoOFF_toggled, connectionType);
+  connect(ui->radioButtonTodos, &QRadioButton::toggled, this, &WidgetEstoque::on_radioButtonTodos_toggled, connectionType);
   connect(ui->table, &TableView::activated, this, &WidgetEstoque::on_table_activated, connectionType);
 }
 
@@ -36,6 +40,46 @@ void WidgetEstoque::setupTables() {
   ui->table->setModel(&model);
 
   ui->table->setItemDelegate(new DoubleDelegate(this));
+
+  //----------------------------------------------
+
+  modelProdutos.setTable("view_produto");
+
+  modelProdutos.setFilter("estoque = TRUE AND descontinuado = FALSE AND desativado = FALSE");
+
+  modelProdutos.proxyModel = new SearchDialogProxyModel(&modelProdutos, this);
+
+  if (not modelProdutos.select()) { return; }
+
+  modelProdutos.setHeaderData("fornecedor", "Fornecedor");
+  modelProdutos.setHeaderData("statusEstoque", "Estoque");
+  modelProdutos.setHeaderData("descricao", "Descrição");
+  modelProdutos.setHeaderData("estoqueRestante", "Estoque Disp.");
+  modelProdutos.setHeaderData("estoqueCaixa", "Estoque Cx.");
+  modelProdutos.setHeaderData("lote", "Lote");
+  modelProdutos.setHeaderData("un", "Un.");
+  modelProdutos.setHeaderData("un2", "Un.2");
+  modelProdutos.setHeaderData("colecao", "Coleção");
+  modelProdutos.setHeaderData("tipo", "Tipo");
+  modelProdutos.setHeaderData("minimo", "Mínimo");
+  modelProdutos.setHeaderData("multiplo", "Múltiplo");
+  modelProdutos.setHeaderData("m2cx", "M/Cx.");
+  modelProdutos.setHeaderData("pccx", "Pç./Cx.");
+  modelProdutos.setHeaderData("kgcx", "Kg./Cx.");
+  modelProdutos.setHeaderData("formComercial", "Form. Com.");
+  modelProdutos.setHeaderData("codComercial", "Cód. Com.");
+  modelProdutos.setHeaderData("precoVenda", "R$");
+  modelProdutos.setHeaderData("validade", "Validade");
+  modelProdutos.setHeaderData("ui", "UI");
+
+  ui->tableProdutos->setModel(&modelProdutos);
+
+  ui->tableProdutos->hideColumn("idProduto");
+  ui->tableProdutos->hideColumn("estoque");
+  ui->tableProdutos->hideColumn("promocao");
+  ui->tableProdutos->hideColumn("descontinuado");
+  ui->tableProdutos->hideColumn("desativado");
+  ui->tableProdutos->hideColumn("representacao");
 }
 
 void WidgetEstoque::setHeaderData() {
@@ -225,6 +269,30 @@ bool WidgetEstoque::gerarExcel(const QString &arquivoModelo, const QString &file
   if (not xlsx.saveAs(fileName)) { return qApp->enqueueException(false, "Ocorreu algum erro ao salvar o arquivo!", this); }
 
   return true;
+}
+
+void WidgetEstoque::on_radioButtonTodos_toggled(bool checked) {
+  if (not checked) { return; }
+
+  modelProdutos.setFilter("estoque = TRUE AND descontinuado = FALSE AND desativado = FALSE");
+
+  if (not modelProdutos.select()) { return; }
+}
+
+void WidgetEstoque::on_radioButtonStaccatoOFF_toggled(bool checked) {
+  if (not checked) { return; }
+
+  modelProdutos.setFilter("estoque = TRUE AND promocao = 2 AND descontinuado = FALSE AND desativado = FALSE");
+
+  if (not modelProdutos.select()) { return; }
+}
+
+void WidgetEstoque::on_radioButtonEstoque_toggled(bool checked) {
+  if (not checked) { return; }
+
+  modelProdutos.setFilter("estoque = TRUE AND promocao = 0 AND descontinuado = FALSE AND desativado = FALSE");
+
+  if (not modelProdutos.select()) { return; }
 }
 
 // NOTE: gerenciar lugares de estoque (cadastro/permissoes)

dcfac190d519b22a2ca590129b9898134fda9b86
Rodrigo Torres <torres.dark@gmail.com>
2020-08-16 21:16:08 -0300

    Adicionado classe NFeDistribuicao

---
+++ src/nfedistribuicao.h
@@ -0,0 +1,42 @@
+#pragma once
+
+#include "sqltablemodel.h"
+
+#include <QWidget>
+
+namespace Ui {
+class NFeDistribuicao;
+}
+
+class NFeDistribuicao : public QWidget {
+  Q_OBJECT
+
+public:
+  explicit NFeDistribuicao(QWidget *parent = nullptr);
+  ~NFeDistribuicao();
+  auto resetTables() -> void;
+  auto updateTables() -> void;
+
+private:
+  // attributes
+  bool isSet = false;
+  bool modelIsSet = false;
+  SqlTableModel model;
+  Ui::NFeDistribuicao *ui;
+  // methods
+  auto buscarNSU() -> void;
+  auto confirmar() -> void;
+  auto darCiencia() -> bool;
+  auto encontraInfCpl(const QString &xml) -> QString;
+  auto encontraTransportadora(const QString &xml) -> QString;
+  auto enviarEvento(const QString &codigoEvento, const QString &operacao) -> bool;
+  auto on_pushButtonCiencia_clicked() -> void;
+  auto on_pushButtonConfirmacao_clicked() -> void;
+  auto on_pushButtonDesconhecimento_clicked() -> void;
+  auto on_pushButtonNaoRealizada_clicked() -> void;
+  auto on_pushButtonPesquisar_clicked() -> void;
+  auto on_table_activated(const QModelIndex &index) -> void;
+  auto pesquisarNFes(const QString &resposta, const QString &idLoja) -> bool;
+  auto setConnections() -> void;
+  auto setupTables() -> void;
+};

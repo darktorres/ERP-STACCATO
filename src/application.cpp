10c2d1228e3850340a79453888bd0b15ac18dbcd
Rodrigo Torres <torres.dark@gmail.com>
2020-09-06 14:35:14 -0300

    Increase version

--- src/application.cpp
+++ src/application.cpp
@@ -16,7 +16,7 @@ Application::Application(int &argc, char **argv, int) : QApplication(argc, argv)
   setOrganizationName("Staccato");
   setApplicationName("ERP");
   setWindowIcon(QIcon("Staccato.ico"));
-  setApplicationVersion("0.8.154");
+  setApplicationVersion("0.8.155");
   setStyle("Fusion");
 
   readSettingsFile();

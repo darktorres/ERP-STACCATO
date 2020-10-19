ea602b7a8e2cfc030a4c3cd2f60c00557a43eaee
Rodrigo Torres <torres.dark@gmail.com>
2020-10-19 11:32:48 -0300

    Increase version
    Alterado telefone nos models Excel/PDF

--- src/application.cpp
+++ src/application.cpp
@@ -20,7 +20,7 @@ Application::Application(int &argc, char **argv, int) : QApplication(argc, argv)
   setOrganizationName("Staccato");
   setApplicationName("ERP");
   setWindowIcon(QIcon("Staccato.ico"));
-  setApplicationVersion("0.9.4");
+  setApplicationVersion("0.9.5");
   setStyle("Fusion");
 
   readSettingsFile();

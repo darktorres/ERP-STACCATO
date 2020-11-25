6ba9db0d333dfb963d942c7c27da0ce0d8f907d2
Rodrigo Torres <torres.dark@gmail.com>
2020-11-25 14:46:55 -0300

    Alterado pasta das imagens no servidor

--- src/searchdialog.cpp
+++ src/searchdialog.cpp
@@ -486,7 +486,7 @@ void SearchDialog::on_pushButtonModelo3d_clicked() {
   const QString fornecedor = model.data(row, "fornecedor").toString();
   const QString codComercial = model.data(row, "codComercial").toString();
 
-  const QString url = "http://" + ip + "/webdav/MODELOS 3D/" + fornecedor + "/" + codComercial + "_3D.skp";
+  const QString url = "http://" + ip + "/webdav/METAIS_VIVIANE/MODELOS 3D/" + fornecedor + "/" + codComercial + ".skp";
 
   auto *manager = new QNetworkAccessManager(this);
   auto request = QNetworkRequest(QUrl(url));

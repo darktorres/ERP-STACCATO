eac0cf5c854e03b7696b6467fa8442bc4439ff54
Rodrigo Torres <torres.dark@gmail.com>
2020-11-28 15:35:00 -0300

    Refeito lógica dos relatórios para filtrar os dados por tipo de funcionário

--- src/sql.h
+++ src/sql.h
@@ -13,4 +13,6 @@ public:
   static auto view_a_receber_vencidos() -> QString;
   static auto view_agendar_entrega(const QString &idVenda = QString(), const QString &status = QString()) -> QString;
   static auto view_entrega_pendente(const QString &filtroBusca = QString(), const QString &filtroCheck = QString(), const QString &filtroStatus = QString()) -> QString;
+  static auto view_relatorio_loja(const QString &mes = QString(), const QString &idUsuario = QString(), const QString &idUsuarioConsultor = QString(), const QString &loja = QString()) -> QString;
+  static auto view_relatorio_vendedor(const QString &mes = QString(), const QString &idUsuario = QString(), const QString &idUsuarioConsultor = QString(), const QString &loja = QString()) -> QString;
 };

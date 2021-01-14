fdbc314abc3468a6d39fd331178955c2a0e6add3
Rodrigo Torres <torres.dark@gmail.com>
2021-01-14 14:58:18 -0300

    Comentar classe WidgetRh por ela não ser utilizada atualmente

--- src/widgetrh.cpp
+++ src/widgetrh.cpp
@@ -13,125 +13,125 @@ WidgetRh::WidgetRh(QWidget *parent) : QWidget(parent), ui(new Ui::WidgetRh) { ui
 
 WidgetRh::~WidgetRh() { delete ui; }
 
-void WidgetRh::resetTables() { modelIsSet = false; }
+// void WidgetRh::resetTables() { modelIsSet = false; }
 
-void WidgetRh::updateTables() {
-  if (not isSet) {
-    ui->dateEdit->setDate(qApp->serverDate());
-    setConnections();
-    isSet = true;
-  }
+// void WidgetRh::updateTables() {
+//  if (not isSet) {
+//    ui->dateEdit->setDate(qApp->serverDate());
+//    setConnections();
+//    isSet = true;
+//  }
 
-  if (not modelIsSet) {
-    setupTables();
-    modelIsSet = true;
-  }
+//  if (not modelIsSet) {
+//    setupTables();
+//    modelIsSet = true;
+//  }
 
-  modelRelatorio.select();
+//  modelRelatorio.select();
 
-  modelTotal.select();
-}
+//  modelTotal.select();
+//}
 
-void WidgetRh::setConnections() {
-  const auto connectionType = static_cast<Qt::ConnectionType>(Qt::AutoConnection | Qt::UniqueConnection);
+// void WidgetRh::setConnections() {
+//  const auto connectionType = static_cast<Qt::ConnectionType>(Qt::AutoConnection | Qt::UniqueConnection);
 
-  connect(ui->dateEdit, &QDateEdit::dateChanged, this, &WidgetRh::on_dateEdit_dateChanged, connectionType);
-  connect(ui->pushButtonSalvarMes, &QPushButton::clicked, this, &WidgetRh::on_pushButtonSalvarMes_clicked, connectionType);
-}
+//  connect(ui->dateEdit, &QDateEdit::dateChanged, this, &WidgetRh::on_dateEdit_dateChanged, connectionType);
+//  connect(ui->pushButtonSalvarMes, &QPushButton::clicked, this, &WidgetRh::on_pushButtonSalvarMes_clicked, connectionType);
+//}
 
-void WidgetRh::setupTables() {
-  modelRelatorio.setTable("comissao");
+// void WidgetRh::setupTables() {
+//  modelRelatorio.setTable("comissao");
 
-  modelRelatorio.setHeaderData("idVenda", "Venda");
+//  modelRelatorio.setHeaderData("idVenda", "Venda");
 
-  ui->table->setModel(&modelRelatorio);
+//  ui->table->setModel(&modelRelatorio);
 
-  ui->table->setItemDelegateForColumn("Faturamento", new ReaisDelegate(this));
-  ui->table->setItemDelegateForColumn("Comissão", new ReaisDelegate(this));
-  ui->table->setItemDelegateForColumn("%", new PorcentagemDelegate(false, this));
+//  ui->table->setItemDelegateForColumn("Faturamento", new ReaisDelegate(this));
+//  ui->table->setItemDelegateForColumn("Comissão", new ReaisDelegate(this));
+//  ui->table->setItemDelegateForColumn("%", new PorcentagemDelegate(false, this));
 
-  ui->table->hideColumn("idVendaProduto1");
-  ui->table->hideColumn("idComissao");
-  ui->table->hideColumn("Mês");
-  ui->table->hideColumn("idUsuario");
+//  ui->table->hideColumn("idVendaProduto1");
+//  ui->table->hideColumn("idComissao");
+//  ui->table->hideColumn("Mês");
+//  ui->table->hideColumn("idUsuario");
 
-  // -------------------------------------------------------------------------
+//  // -------------------------------------------------------------------------
 
-  modelTotal.setTable("view_relatorio_vendedor");
+//  modelTotal.setTable("view_relatorio_vendedor");
 
-  ui->tableTotal->setModel(&modelTotal);
+//  ui->tableTotal->setModel(&modelTotal);
 
-  ui->tableTotal->setItemDelegateForColumn("Faturamento", new ReaisDelegate(this));
-  ui->tableTotal->setItemDelegateForColumn("Comissão", new ReaisDelegate(this));
-  ui->tableTotal->setItemDelegateForColumn("%", new PorcentagemDelegate(false, this));
+//  ui->tableTotal->setItemDelegateForColumn("Faturamento", new ReaisDelegate(this));
+//  ui->tableTotal->setItemDelegateForColumn("Comissão", new ReaisDelegate(this));
+//  ui->tableTotal->setItemDelegateForColumn("%", new PorcentagemDelegate(false, this));
 
-  ui->tableTotal->hideColumn("idUsuario");
-  ui->tableTotal->hideColumn("Mês");
-}
+//  ui->tableTotal->hideColumn("idUsuario");
+//  ui->tableTotal->hideColumn("Mês");
+//}
 
-void WidgetRh::on_pushButtonSalvarMes_clicked() {
-  const QDate mes = ui->dateEdit->date();
+// void WidgetRh::on_pushButtonSalvarMes_clicked() {
+//  const QDate mes = ui->dateEdit->date();
 
-  SqlQuery queryVerifica;
-  queryVerifica.prepare("SELECT * FROM comissao WHERE Mês = :mes");
-  queryVerifica.bindValue(":mes", mes.toString("yyyy-MM"));
+//  SqlQuery queryVerifica;
+//  queryVerifica.prepare("SELECT * FROM comissao WHERE Mês = :mes");
+//  queryVerifica.bindValue(":mes", mes.toString("yyyy-MM"));
 
-  if (not queryVerifica.exec()) { throw RuntimeException("Erro buscando dados da comissão: " + queryVerifica.lastError().text(), this); }
+//  if (not queryVerifica.exec()) { throw RuntimeException("Erro buscando dados da comissão: " + queryVerifica.lastError().text(), this); }
 
-  if (queryVerifica.first()) {
-    QMessageBox msgBox(QMessageBox::Question, "Atenção!", "Este mês já está cadastrado. Deseja substituir os dados?", QMessageBox::Yes | QMessageBox::No, this);
-    msgBox.setButtonText(QMessageBox::Yes, "Substituir");
-    msgBox.setButtonText(QMessageBox::No, "Voltar");
+//  if (queryVerifica.first()) {
+//    QMessageBox msgBox(QMessageBox::Question, "Atenção!", "Este mês já está cadastrado. Deseja substituir os dados?", QMessageBox::Yes | QMessageBox::No, this);
+//    msgBox.setButtonText(QMessageBox::Yes, "Substituir");
+//    msgBox.setButtonText(QMessageBox::No, "Voltar");
 
-    if (msgBox.exec() == QMessageBox::No) { return; }
+//    if (msgBox.exec() == QMessageBox::No) { return; }
 
-    SqlQuery queryRemove;
-    queryRemove.prepare("DELETE FROM comissao WHERE Mês = :mes");
-    queryRemove.bindValue(":mes", mes.toString("yyyy-MM"));
+//    SqlQuery queryRemove;
+//    queryRemove.prepare("DELETE FROM comissao WHERE Mês = :mes");
+//    queryRemove.bindValue(":mes", mes.toString("yyyy-MM"));
 
-    if (not queryRemove.exec()) { throw RuntimeException("Erro removendo dados: " + queryRemove.lastError().text(), this); }
-  }
+//    if (not queryRemove.exec()) { throw RuntimeException("Erro removendo dados: " + queryRemove.lastError().text(), this); }
+//  }
 
-  SqlQuery queryRelatorio;
-  queryRelatorio.prepare("SELECT * FROM view_relatorio WHERE Mês = :mes");
-  queryRelatorio.bindValue(":mes", mes.toString("yyyy-MM"));
+//  SqlQuery queryRelatorio;
+//  queryRelatorio.prepare("SELECT * FROM view_relatorio WHERE Mês = :mes");
+//  queryRelatorio.bindValue(":mes", mes.toString("yyyy-MM"));
 
-  // get data from view_relatorio
-  if (not queryRelatorio.exec()) { throw RuntimeException("Erro buscando dados da comissão: " + queryRelatorio.lastError().text(), this); }
+//  // get data from view_relatorio
+//  if (not queryRelatorio.exec()) { throw RuntimeException("Erro buscando dados da comissão: " + queryRelatorio.lastError().text(), this); }
 
-  if (not queryRelatorio.first()) { return qApp->enqueueInformation("Não há dados para este mês!", this); }
+//  if (not queryRelatorio.first()) { return qApp->enqueueInformation("Não há dados para este mês!", this); }
 
-  // insert last month data into 'comissao'
-  for (int i = 0; i < queryRelatorio.size(); ++i, queryRelatorio.next()) {
-    int newRow = modelRelatorio.insertRowAtEnd();
+//  // insert last month data into 'comissao'
+//  for (int i = 0; i < queryRelatorio.size(); ++i, queryRelatorio.next()) {
+//    int newRow = modelRelatorio.insertRowAtEnd();
 
-    modelRelatorio.setData(newRow, "Loja", queryRelatorio.value("Loja"));
-    modelRelatorio.setData(newRow, "Vendedor", queryRelatorio.value("Vendedor"));
-    modelRelatorio.setData(newRow, "idUsuario", queryRelatorio.value("idUsuario"));
-    modelRelatorio.setData(newRow, "idVenda", queryRelatorio.value("idVenda"));
-    modelRelatorio.setData(newRow, "Mês", queryRelatorio.value("Mês"));
-    modelRelatorio.setData(newRow, "Faturamento", queryRelatorio.value("Faturamento"));
-    modelRelatorio.setData(newRow, "Comissão", queryRelatorio.value("Comissão"));
-    modelRelatorio.setData(newRow, "%", queryRelatorio.value("%"));
-  }
+//    modelRelatorio.setData(newRow, "Loja", queryRelatorio.value("Loja"));
+//    modelRelatorio.setData(newRow, "Vendedor", queryRelatorio.value("Vendedor"));
+//    modelRelatorio.setData(newRow, "idUsuario", queryRelatorio.value("idUsuario"));
+//    modelRelatorio.setData(newRow, "idVenda", queryRelatorio.value("idVenda"));
+//    modelRelatorio.setData(newRow, "Mês", queryRelatorio.value("Mês"));
+//    modelRelatorio.setData(newRow, "Faturamento", queryRelatorio.value("Faturamento"));
+//    modelRelatorio.setData(newRow, "Comissão", queryRelatorio.value("Comissão"));
+//    modelRelatorio.setData(newRow, "%", queryRelatorio.value("%"));
+//  }
 
-  modelRelatorio.submitAll();
+//  modelRelatorio.submitAll();
 
-  // -------------------------------------------------------------------------
+//  // -------------------------------------------------------------------------
 
-  updateTables();
+//  updateTables();
 
-  qApp->enqueueInformation("Dados salvos com sucesso!", this);
-}
+//  qApp->enqueueInformation("Dados salvos com sucesso!", this);
+//}
 
-void WidgetRh::on_dateEdit_dateChanged(const QDate &date) {
-  modelRelatorio.setFilter("Mês = '" + date.toString("yyyy-MM") + "'");
+// void WidgetRh::on_dateEdit_dateChanged(const QDate &date) {
+//  modelRelatorio.setFilter("Mês = '" + date.toString("yyyy-MM") + "'");
 
-  modelRelatorio.select();
+//  modelRelatorio.select();
 
-  // -------------------------------------------------------------------------
+//  // -------------------------------------------------------------------------
 
-  modelTotal.setFilter("Mês = '" + date.toString("yyyy-MM") + "'");
+//  modelTotal.setFilter("Mês = '" + date.toString("yyyy-MM") + "'");
 
-  modelTotal.select();
-}
+//  modelTotal.select();
+//}

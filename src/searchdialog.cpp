0f364454ad3b6791b1262445adc2d7878420a367
Rodrigo Torres <torres.dark@gmail.com>
2021-06-16 16:36:45 -0300

    Hide columns

--- src/searchdialog.cpp
+++ src/searchdialog.cpp
@@ -217,7 +217,7 @@ SearchDialog *SearchDialog::loja(QWidget *parent) {
   sdLoja->ui->table->setItemDelegateForColumn("valorMinimoFrete", new ReaisDelegate(parent));
 
   sdLoja->hideColumns({"idLoja", "codUF", "desativado", "certificadoSerie", "certificadoSenha", "porcentagemPIS", "porcentagemCOFINS", "custoTransporteTon", "custoTransporte1", "custoTransporte2",
-                       "custoFuncionario"});
+                       "custoFuncionario", "ultimoNSU", "maximoNSU"});
 
   sdLoja->setHeaderData("descricao", "Descrição");
   sdLoja->setHeaderData("nomeFantasia", "Nome Fantasia");

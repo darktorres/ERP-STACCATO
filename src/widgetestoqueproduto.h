f390ac7d6817f774f92a6b0649c6aa91a697f56c
Rodrigo Torres <torres.dark@gmail.com>
2021-06-18 02:06:14 -0300

    Separar WidgetEstoque em um widget por aba

---
+++ src/widgetestoqueproduto.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "sqltablemodel.h"
+
+#include <QTimer>
+#include <QWidget>
+
+namespace Ui {
+class WidgetEstoqueProduto;
+}
+
+class WidgetEstoqueProduto : public QWidget {
+  Q_OBJECT
+
+public:
+  explicit WidgetEstoqueProduto(QWidget *parent = nullptr);
+  ~WidgetEstoqueProduto();
+
+  auto resetTables() -> void;
+  auto updateTables() -> void;
+
+private:
+  // attributes
+  bool isSet = false;
+  bool modelIsSet = false;
+  QTimer timer;
+  SqlTableModel modelProdutos;
+  Ui::WidgetEstoqueProduto *ui;
+  // methods
+  auto delayFiltro() -> void;
+  auto montaFiltro() -> void;
+  auto on_radioButtonEstoque_toggled(bool checked) -> void;
+  auto on_radioButtonStaccatoOFF_toggled(bool checked) -> void;
+  auto on_radioButtonTodos_toggled(bool checked) -> void;
+  auto setConnections() -> void;
+  auto setupTables() -> void;
+};

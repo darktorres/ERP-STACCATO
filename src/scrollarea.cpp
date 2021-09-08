d3116694e3a6f308b1b6e4da8b6299ed953a3035
Rodrigo Torres <torres.dark@gmail.com>
2021-09-08 17:00:06 -0300

    Ajustes visuais

--- src/scrollarea.cpp
+++ src/scrollarea.cpp
@@ -5,4 +5,6 @@
 
 ScrollArea::ScrollArea(QWidget *parent) : QScrollArea(parent) {}
 
-QSize ScrollArea::sizeHint() const { return QScrollArea::sizeHint() + QSize(verticalScrollBar()->sizeHint().width(), 0) + QSize(horizontalScrollBar()->sizeHint().width(), 0); }
+QSize ScrollArea::sizeHint() const { return QScrollArea::sizeHint() + QSize(verticalScrollBar()->sizeHint().width(), 0); }
+
+QSize ScrollArea::minimumSizeHint() const { return sizeHint(); }

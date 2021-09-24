454d45d1b7d63de181c38c665c04fe9f746bf157
Rodrigo Torres <torres.dark@gmail.com>
2021-09-24 17:17:30 -0300

    Filtrar por fornecedor nas telas de Compras

--- src/widgetcompraconfirmar.h
+++ src/widgetcompraconfirmar.h
@@ -30,6 +30,7 @@ private:
   auto on_pushButtonCancelarCompra_clicked() -> void;
   auto on_pushButtonConfirmarCompra_clicked() -> void;
   auto on_pushButtonFollowup_clicked() -> void;
+  auto on_tableResumo_clicked(const QModelIndex &index) -> void;
   auto setConnections() -> void;
   auto setupTables() -> void;
 };

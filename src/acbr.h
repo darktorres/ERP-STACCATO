4d5950c0ecf14e910be075b8c7b8c063137473b9
Rodrigo Torres <torres.dark@gmail.com>
2022-01-21 11:55:18 -0300

    ACBr agora é sempre remoto
    ACBr agora é sempre remoto

--- src/acbr.h
+++ src/acbr.h
@@ -12,7 +12,7 @@ public:
   ~ACBr() final = default;
 
   auto consultarNFe(const int idNFe) -> std::tuple<QString, QString>;
-  auto enviarComando(const QString &comando, const bool local = false) -> QString;
+  auto enviarComando(const QString &comando) -> QString;
   auto enviarEmail(const QString &emailDestino, const QString &emailCopia, const QString &assunto, const QString &filePath) -> void;
 
 private:
